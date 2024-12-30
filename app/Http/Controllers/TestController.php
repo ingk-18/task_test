98686ba9208b82720113a7291f0a32b48e29b111
ingk-18 <readog1224@yahoo.co.jp>
2024-12-31 00:56:38 +0900

    クエリビルダ、エロケントでデータ取得

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,13 +2,25 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Illuminate\Suppot\Facades\DB;
 use App\Models\Test;
 
 class TestController extends Controller
 {
     public function index(){
         $hoge = Test::all();
+
+        $foo = Test::findOrFail(1);
+
+        // エロケント
+        $bar = Test::where('text', '=', 'hogehoge')
+            ->get();
+
+        // クエリビルダ
+        DB::table('tests')->where('test', '=', 'foofoo')
+            ->get();
+
+        dd($hoge, $foo, $bar);
 
         return view('tests.test', compact('hoge'));
 
